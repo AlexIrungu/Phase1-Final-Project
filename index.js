f4e1cadf961c2716d453f97de76903619202e9c3
AlexIrungu <aleaxmuiruri@gmail.com>
2022-12-15 11:30:08 +0300

    Added the search button and search bar

---
+++ index.js
@@ -0,0 +1,9 @@
+let url = 'https://api.openbrewerydb.org/breweries/random'
+const button = document.querySelector('.container button')
+const drinkText = document.querySelector('.container p')
+
+button.addEventListener('click',getDrink)
+
+function getDrink(){
+
+}
