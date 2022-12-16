98fe2fe02b48b65e4ed7f0d495fd01fc5a12512e
AlexIrungu <aleaxmuiruri@gmail.com>
2022-12-16 09:39:39 +0300

    Starting DOM Manipulation for the html login form

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-let url = 'https://api.openbrewerydb.org/breweries/random'
-const button = document.querySelector('.container button')
-const drinkText = document.querySelector('.container p')
+// let url = 'https://api.openbrewerydb.org/breweries/random'
+// // const button = document.querySelector('.container button')
+// // //const drinkText = document.querySelector('.container p')
 
-button.addEventListener('click',getDrink)
+// // button.addEventListener('click',getDrink)
 
-function getDrink(){
+// // function getDrink(){
 
-}
+// // }
+// const name = document.getElementById('name')
+// const password = document.getElementById('password')
+// const form = document.getElementById('form')
+
+// form.addEventListener('submit',(e) => {
+//     let messages = []
+    
+// })
+
+// document.addEventListener('DOMContentLoaded',init)
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('form').addEventListener('submit', (e) => {
+        //we console log and the page refreshes. 
+        //console.log(e)
+        //we then add a prevent event to remove the default behaviour of the html form
+        e.preventDefault()
+        //we console log to check the submit event
+        console.log(e.target);
+    })
+})
