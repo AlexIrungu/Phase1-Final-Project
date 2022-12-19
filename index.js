dc264e46298db53ca69f6569e30773526d78e0b7
AlexIrungu <aleaxmuiruri@gmail.com>
2022-12-19 13:06:15 +0300

    Introduced a css framework: bookstrap.Started on DOM manipulation

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-// let url = 'https://api.openbrewerydb.org/breweries/random'
-// // const button = document.querySelector('.container button')
-// // //const drinkText = document.querySelector('.container p')
+ //let url = 'https://api.openbrewerydb.org/breweries/random'
+//to listen to DOM loading event
+document.addEventListener('DOMContentLoaded', () => {
+    //create random drink element
+    const createRandomDrink = (type, title, city) => {
+        //creating card div
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add('card', 'col-20')
 
-// // button.addEventListener('click',getDrink)
+        const rowDiv = document.createElement('div')
+        rowDiv.classList.add('row')
+    }
+})
 
-// // function getDrink(){
-
-// // }
-// const name = document.getElementById('name')
-// const password = document.getElementById('password')
-// const form = document.getElementById('form')
-
-// form.addEventListener('submit',(e) => {
-//     let messages = []
-    
-// })
-
-// document.addEventListener('DOMContentLoaded',init)
-
+//form dom manipulation
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('form').addEventListener('submit', (e) => {
         //we console log and the page refreshes. 
@@ -25,6 +20,18 @@ document.addEventListener('DOMContentLoaded', () => {
         //we then add a prevent event to remove the default behaviour of the html form
         e.preventDefault()
         //we console log to check the submit event
-        console.log(e.target);
+        console.log(e.target.enter_email.value)
     })
 })
+
+function handleEmail(email){
+    //console.log(email)
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.textContent = 'x'
+    p.appendChild(btn)
+    p.textContent = `${email}`
+    //console.log(p);
+    document.querySelector('#email_container').appendChild(p)
+}
+
